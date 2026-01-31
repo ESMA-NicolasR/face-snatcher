75dbff497d60c205c300f7e0f019df42132dcf1b
Alkador-git <brutalityalkador@gmail.com>
2026-01-31 01:56:48 +0100

    Right and left path

--- Alois/PNJ_Manager.gd
+++ Alois/PNJ_Manager.gd
@@ -1,29 +1,49 @@
-extends Path3D
+extends Node
 
-# 1. Tes variables en haut
+# Mesh pnj
 @export var PNJ_mesh_scene: PackedScene = preload("res://Alois/pnj_mesh.tscn")
+# Vitesse de déplacement m/s
 @export var vitesse: float = 5.0
-
-func _on_timer_timeout() -> void:
-	print("spawn")
-	pnj_spawn()
+# Premier chemin à assigner dans l'inspecteur
+@export var path_Left: Path3D
+# Deuxième chemin à assigner dans l'inspecteur
+@export var path_Right: Path3D
 
 func pnj_spawn():
+	# Choisir un chemin aléatoirement entre les deux
+	var chemins = [path_Left, path_Right]
+	var chemin_choisi = chemins.pick_random()
+	
+	# Sécurité si un chemin n'est pas assigné
+	if chemin_choisi == null: return
+
 	# Création suiveur de path
 	var new_victim = PathFollow3D.new()
 	new_victim.loop = false
-	add_child(new_victim)
+	# On ajoute le suiveur au chemin choisi aléatoirement
+	chemin_choisi.add_child(new_victim)
 	
 	# Instance mesh dans le suiveur de path
 	var new_pnj = PNJ_mesh_scene.instantiate()
 	new_victim.add_child(new_pnj)
 
 func _process(delta: float) -> void:
-	# Faire avancer les pnj dans le path
-	for pnj in get_children():
-		if pnj is PathFollow3D:
-			pnj.progress += vitesse * delta
-			
-			# Supprimer le PNJ s'il arrive au bout du path
-			if pnj.progress_ratio >= 1:
-				pnj.queue_free()
+	# Liste des chemins à traiter
+	var chemins = [path_Left, path_Right]
+	
+	for p in chemins:
+		if p == null: continue
+		
+		# Faire avancer les pnj dans chaque path
+		for pnj in p.get_children():
+			if pnj is PathFollow3D:
+				pnj.progress += vitesse * delta
+				
+				# Supprimer le PNJ s'il arrive au bout du path
+				if pnj.progress_ratio >= 0.98:
+					pnj.queue_free()
+
+
+func _on_pnj_spawn_timer_timeout() -> void:
+	print("spawn")
+	pnj_spawn()
